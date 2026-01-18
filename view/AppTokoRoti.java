984be0ca5125e753a680eac708657d19b2db08c8
iqbalhidayatur <125681388+iqbalhidayatur@users.noreply.github.com>
2026-01-18 21:46:02 +0700

    Add files via upload

---
+++ view/AppTokoRoti.java
@@ -0,0 +1,9 @@
+package com.mycompany.apptokoroti;
+public class AppTokoRoti {
+
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(() -> {
+        new UI().setVisible(true);
+        });
+    }
+}
