b5159911467f321f47ee811d51045f96654c235a
iqbalhidayatur <125681388+iqbalhidayatur@users.noreply.github.com>
2026-01-18 21:58:32 +0700

    Delete view directory

--- view/AppTokoRoti.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.apptokoroti;
-public class AppTokoRoti {
-
-    public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(() -> {
-        new UI().setVisible(true);
-        });
-    }
-}
