678c0f86ac79db4a75b757876334e6069816f4df
iqbalhidayatur <125681388+iqbalhidayatur@users.noreply.github.com>
2026-01-22 14:49:09 +0700

    Add files via upload

---
+++ view/AppTokoRoti/src/main/java/com/mycompany/apptokoroti/AppTokoRoti.java
@@ -0,0 +1,7 @@
+package com.mycompany.apptokoroti;
+public class AppTokoRoti {
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> new UI_RoleSelect().setVisible(true));
+    }
+}
